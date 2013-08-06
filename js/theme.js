17451764f6542a00f4d84523341d6e091f813c60
alexander-chepur <chepur@mail.ru>
2013-08-06 22:41:19 +0400

    Slides in About section can now have more complex structure

--- js/theme.js
+++ js/theme.js
@@ -205,12 +205,20 @@ readyState(function()
      * Flexslider
      */
     $('.slide-content').height($(window).height()*0.55); 
-    $('.slide-content').css({'background-image': 
+    $('.slide-content > div').css({'background-image': 
         function() 
         {
             return 'url("'+ $(this).attr('rel') +'")'; 
         }
     });
+    $('.slide-content > div').css({'background-position': 
+        function() 
+        {
+            return $(this).attr('pos'); 
+        }
+    });
+    $('.slide-content > div').css({'background-size': 'contain'});
+    $('.slide-content > div').css({'background-repeat': 'no-repeat'});
     $('.flexslider').flexslider();
 
     /**
