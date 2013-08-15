335719275984cc7d4587c6cf485886924c1b1517
alexander-chepur <chepur@mail.ru>
2013-08-15 13:42:00 +0400

    Release version

---
+++ index.php
@@ -0,0 +1,7 @@
+﻿<?php
+//header("HTTP/1.0 404 Not Found");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: ".date(DATE_RFC822, mktime(date("H"), date("i")-1, date("s"), date("n"),   date("j"),   date("Y")))); // Just expired
+header("Last-Modified: ".date(DATE_RFC822, mktime(date("H"), date("i")-1, date("s"), date("n"),   date("j"),   date("Y")))); // Just updated
+readfile("index.html");
+?>
---
+++ sitemap.php
@@ -0,0 +1,19 @@
+﻿<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <url>
+      <loc>http://taichen.ru/</loc>
+      <lastmod></lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1</priority>
+   </url>
+</urlset>
+XML;
+
+$xml = new SimpleXMLElement($xmlstr);
+
+$xml->url[0]->lastmod = date("Y")."-".date("m")."-".date("d"); 
+header("Content-Type: text/xml");
+echo $xml->asXML(); 
+?>
